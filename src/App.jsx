f3d7ea8f4f1c4a90eea37d94cb52cb723cf0324d
harshil3134 <harshilpatel2223@gmail.com>
2025-05-30 23:53:22 +0530

    fixing deployment err

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
-import Game from "./components/game";
+import Game from "./components/Game.jsx";
+
 
 
 function App() {
 return(
   <>
-
   <Game/>
   </>
 )
