32c085c2486174ed2f928f78913bf3e991538676
harshil3134 <harshilpatel2223@gmail.com>
2025-05-30 23:32:44 +0530

    removed unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import Game from "./components/game";
-import { Cam } from "./components/Cam";
+
 
 function App() {
 return(
   <>
-  {/* <Cam/> */}
+
   <Game/>
   </>
 )
