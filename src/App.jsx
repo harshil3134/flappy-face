f3054893acd85d07e75c63c2c2825f8a2a451686
harshil3134 <harshilpatel2223@gmail.com>
2025-05-31 22:58:12 +0530

    added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import Game from "./components/Game.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 
 
 function App() {
 return(
   <>
+  <Analytics/>
   <Game/>
   </>
 )
